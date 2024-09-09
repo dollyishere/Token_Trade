347f90c681e33555ef629defe38f03913c83fcbc
dollyishere <rundollyrun8@gmail.com>
2024-09-10 05:34:53 +0900

    feat: views, tests, serializers 폴더 세팅 및 order, product serializer 생성

---
+++ resource_server/order/serializers/order_detail_serializer.py
@@ -0,0 +1,30 @@
+from rest_framework.serializers import ModelSerializer
+from product.serializers import ProductSerializer
+from order.models import Order
+
+
+class OrderDetailSerializer(ModelSerializer):
+    """Serializer for order details"""
+
+    product = ProductSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "order_number",
+            "customer",
+            "status",
+            "quantity",
+            "shipping_address",
+            "product",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at",
+        ]
---
+++ resource_server/product/serializers/product_serializer.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import ModelSerializer
+from product.models import Product
+
+
+class ProductSerializer(ModelSerializer):
+    """Serializer for Product(Gold)"""
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "product_name",
+            "manager",
+            "purity",
+            "price_per_gram",
+            "crr_stock_per_gram",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at",
+        ]
